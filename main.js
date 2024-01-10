0b9c77b639efd17f9db822769843ddc3fcdbbaaf
Jhonathan <Jhonathan.Engineer@gmail.com>
2024-01-10 16:37:18 -0300

    chore: add function show and hide the form

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
    $('header button').click(function(){
-    alert('Expandir galeria')
+      $('form').slideDown()
    })
 
-   $('form').on('submit', function(e) {
-    e.preventDefault();
+   $('#botao-cancelar').click(function(){
+      $('form').slideUp()
    })
 })
