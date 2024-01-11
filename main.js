3e37e592b9a9271f3be8c425e4271e86129de1d3
Jhonathan <Jhonathan.Engineer@gmail.com>
2024-01-11 15:05:43 -0300

    chore: add modify in mainjs

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
-$(document).ready(function(){
+$(document).ready(function (){
    $('header button').click(function(){
       $('form').slideDown()
    })
 
    $('#botao-cancelar').click(function(){
       $('form').slideUp()
+   })
+
+   $('form').on('submit', function(e){
+      e.preventDefault()
+
    })
 })
