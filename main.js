eadb346811e30e2b032eb1c21738892a5ac7fd80
Jhonathan <Jhonathan.Engineer@gmail.com>
2024-01-29 14:52:52 -0300

    ADD: evento ao clicar no botao nova imagem e tirei o recarregamento da pagina ao clicar em adicionar

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
 $(document).ready(function() {
-   alert('Olá Jquery')
+   $('header button').click(function(){
+      alert('teste')
+   })
+
+   $('form').on('submit', function(e){
+      e.preventDefault()
+   })
 })
