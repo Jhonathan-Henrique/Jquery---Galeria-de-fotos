73c0f9773c54af7f09bbe4b7d76085554bc36c60
Jhonathan <Jhonathan.Engineer@gmail.com>
2024-01-30 11:15:44 -0300

    chore: projeto finalizado

--- main.js
+++ main.js
@@ -5,6 +5,18 @@ $(document).ready(function() {
 
    $('form').on('submit', function(e){
       e.preventDefault()
+      const enderecoDaNovaImg = $('#endereco-img-nova').val()
+      const novoItem = $('<li style="display: none"></li>')
+      $(`<img src="${enderecoDaNovaImg}" />`).appendTo(novoItem)
+      $(`
+         <div class="overlay-link-img">
+            <a href="${enderecoDaNovaImg}" title="Ver imagem em tamanho real" target="_blank">
+               Ver imagem em tamanho real
+            </a>
+         </div>`).appendTo(novoItem)
+         $(novoItem).appendTo('ul')
+         $(novoItem).fadeIn(1000)
+         $('#endereco-img-nova').val(' ')
    })
 
    $('#btn-cancelar').click(function(){
